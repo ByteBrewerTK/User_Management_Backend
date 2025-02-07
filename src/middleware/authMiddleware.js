bc041fc9c906c646c236d376747a820b8c176bf9
Abutalif Khan <talifkhan1@hotmail.com>
2025-02-07 23:57:12 +0530

    add - middleware

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "../config/dotenv.js";
+
+const authMiddleware = (req, res, next) => {
+	const token = req.header("x-auth-token");
+	if (!token) return res.status(401).json({ message: "Access denied" });
+
+	try {
+		const decoded = jwt.verify(token, JWT_SECRET);
+		req.user = decoded;
+		next();
+	} catch (err) {
+		res.status(400).json({ message: "Invalid token" });
+	}
+};
+
+export default authMiddleware;
