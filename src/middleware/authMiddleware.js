a44667039e8eb431e8698d095d3c853251ae634e
Abutalif Khan <talifkhan1@hotmail.com>
2025-02-08 23:59:22 +0530

    update - get token with cookie

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "../config/dotenv.js";
 
 const authMiddleware = (req, res, next) => {
-	const token = req.header("x-auth-token");
+	const token = req.cookies.token || req.header("x-auth-token");
 	if (!token) return res.status(401).json({ message: "Access denied" });
 
 	try {
