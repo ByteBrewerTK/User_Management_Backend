924af3cf229814bc9b2f6598e6de958d7a9fbcc2
Abutalif Khan <talifkhan1@hotmail.com>
2025-02-08 23:56:21 +0530

    update - cookie parser and routes, db sync

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import cors from "cors";
 import db from "./config/db.js";
-import userRoutes from "./routes/user.routes.js"; // User routes
+import userRoutes from "./routes/user.routes.js";
+import roleRoutes from "./routes/role.routes.js";
 import sequelize from "./config/db.js";
 
 dotenv.config();
@@ -10,15 +12,16 @@ const app = express();
 
 // Middleware
 app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
 
 // Routes
+app.use("/api/v1/roles", roleRoutes);
 app.use("/api/v1/users", userRoutes);
 
-// Test Database Connection
 sequelize
-	.sync({ force: false })
-	.then(() => console.log("Database synced successfully"))
+	.sync({ alter: false })
+	.then(() => console.log("Database tables synced"))
 	.catch((err) => console.error("Error syncing database:", err));
 
 db.authenticate()
