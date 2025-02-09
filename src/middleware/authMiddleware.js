caf20853c481364b4b4921dc4565e50e6f9e842e
Abutalif Khan <talifkhan1@hotmail.com>
2025-02-09 23:56:15 +0530

    update - auth middleware and add authorization middleware

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,17 +1,39 @@
 import jwt from "jsonwebtoken";
-import { JWT_SECRET } from "../config/dotenv.js";
+import User from "../models/user.model.js";
+import Role from "../models/role.model.js";
 
-const authMiddleware = (req, res, next) => {
+export const authMiddleware = async (req, res, next) => {
 	const token = req.cookies.token || req.header("x-auth-token");
 	if (!token) return res.status(401).json({ message: "Access denied" });
 
 	try {
-		const decoded = jwt.verify(token, JWT_SECRET);
-		req.user = decoded;
+		const decoded = jwt.verify(token, process.env.JWT_SECRET);
+		const user = await User.findByPk(decoded.id, {
+			include: [
+				{
+					model: Role,
+					attributes: ["id", "name"],
+				},
+			],
+		});
+
+		req.user = user;
+
 		next();
 	} catch (err) {
 		res.status(400).json({ message: "Invalid token" });
 	}
 };
 
-export default authMiddleware;
+export const authorizeRoles = (...allowedRoles) => {
+	return (req, res, next) => {
+		const user = req.user;
+		console.log(user.Role.name);
+		if (!user || !allowedRoles.includes(user.Role.name)) {
+			return res
+				.status(403)
+				.json({ message: "Forbidden: Insufficient permissions" });
+		}
+		next();
+	};
+};
