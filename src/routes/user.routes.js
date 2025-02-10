4316fb8ac9055309a2ecab34559b5e60ec784d4b
Abutalif Khan <talifkhan1@hotmail.com>
2025-02-10 19:33:26 +0530

    update - multer middleware for file multipart form data

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -10,13 +10,14 @@ import {
 	authMiddleware,
 	authorizeRoles,
 } from "../middleware/authMiddleware.js";
+import upload from "../../utils/multer.js";
 
 const router = Router();
 
 router.use(authMiddleware);
 
 // User Routes
-router.post("/", authorizeRoles("Admin"), addUser);
+router.post("/", authorizeRoles("Admin"),upload.single('avatar'), addUser);
 router.get("/", authorizeRoles("Admin"), getAllUsers);
 router.get("/:id", authorizeRoles("Admin"), getUserById);
 router.put("/:id", authorizeRoles("Admin"), updateUser);
